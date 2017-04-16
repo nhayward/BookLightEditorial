c7c08f492bcc62cf72c83378ab418c3efc94b181
Nick Hayward <nhayward2011@gmail.com>
2017-04-15 22:00:59 -0400

    making contact forms post to gsheet on submit and adding back button to quick contact thank you page

--- user/themes/illuminate/js/illuminate.js
+++ user/themes/illuminate/js/illuminate.js
@@ -4,5 +4,17 @@ $(document).ready(function() {
 		$('#service-select option').removeAttr('selected');
 		$('#service-select option[value=' + $(this).attr('data-select') +' ]').attr('selected', 'selected');
 	});
+
+	$('#contact-form, #quick-contact-form').submit(function() {
+		var url = "https://script.google.com/macros/s/AKfycbz5QG_vIto9CssV9hKWBazzSYEKvtoXVZs0lo3tf37Githsy0RZ/exec";
+		var formData = $(this).serializeArray();
+		var data = {
+			"Name": formData[0].value,
+			"Email": formData[1].value,
+			"Service": formData[2].value,
+			"Message": formData[3].value
+		};
+		$.post(url, data);
+	});
 	
 });
